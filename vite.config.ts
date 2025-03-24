4c664d2379cfbec3bab94c3f5ffd43892be513ad
soumi0705 <somida00@gmail.com>
2025-03-25 07:47:22 +1100

    Added React in Webview and Created Modifications

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// @ts-expect-error Too lazy
+import path from "path";
+// https://github.com/tailwindlabs/tailwindcss/issues/16751
+// @ts-expect-error Tailwind and Vite are having some problems ATM
+import tailwind from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwind()],
+  root: path.join(__dirname, "game"), // Point to your app directory
+  build: {
+    outDir: path.join(__dirname, "webroot"), // Specify your desired output directory
+    emptyOutDir: true, // Clean the output directory before each build
+    copyPublicDir: true, // Copies over assets
+    sourcemap: true, // Enable sourcemaps
+  },
+});
