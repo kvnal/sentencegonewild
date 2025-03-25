27d7084e40efaca344ddd788d48838090e82ad4f
kvnal <72142540+kvnal@users.noreply.github.com>
2025-03-25 09:44:50 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -85,7 +85,6 @@ Devvit.addCustomPostType({
             // do the redis? and create a comment
             // changeview to check submitted page
             console.log("User completed sentence",data.payload.completedSentence);
-          console.log("user completed sentence",message.userSentence);
             console.log("postid",_context.postId)
 
             if(_context.postId){
